9f42fedf082ca47931eb6f6419acf492a8ce7c35
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-06-21 17:41:55 +0530

    upload middleware error fixex

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -30,6 +30,7 @@ const checkFileType = (file, cb) => {
 // 3. Initialize Multer
 const upload = multer({
   storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   },
--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,12 @@ startCronJobs();
 const __dirname = path.resolve();
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ 
+  origin: function(origin, callback) {
+    callback(null, true);
+  }, 
+  credentials: true 
+}));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
