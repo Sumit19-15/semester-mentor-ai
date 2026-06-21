333358bbf87d77348257b9d2d33770aa70a8f0b4
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-06-21 15:45:53 +0530

    *

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ startCronJobs();
 const __dirname = path.resolve();
 
 const app = express();
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
